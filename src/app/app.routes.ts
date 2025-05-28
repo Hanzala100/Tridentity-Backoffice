d0b8b91c0ccd4d12170d8e1ba8ab63a674cf09dc
Hanzala Sarguroh <hanzala.sarguroh@tribox.me>
2025-05-28 23:25:33 +0530

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+
+  {
+    path: '',
+    loadComponent: () => import('./home/home.page').then(m => m.HomePage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then(m => m.LoginPage)
+  },
+];
